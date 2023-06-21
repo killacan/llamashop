d4cb07282b7725b86c505b295e58891aeeb4b4e1
Leisurellama <kameron000@yahoo.com>
2023-06-21 00:50:39 -0500

    add clerk and middlewear

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  return (
+
+    <ClerkProvider {...pageProps}>
+
+      <Component {...pageProps} />
+
+    </ClerkProvider>
+
+  );
+
+}
 
 export default api.withTRPC(MyApp);
