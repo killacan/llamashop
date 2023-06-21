31fb584ad17d24b08765820fedf15055a6999dbd
Leisurellama <kameron000@yahoo.com>
2023-06-21 11:58:39 -0500

    fixed automatic redirect to login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+    afterAuth(auth, req, evt) {
+        // Users who are not authenticated will stay on the page they are on. 
+            return
+    }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
