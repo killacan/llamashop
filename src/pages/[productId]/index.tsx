86eff91bdd7f97c38e7b2666a1f1a835b83921c6
Leisurellama <kameron000@yahoo.com>
2023-08-06 22:22:52 -0500

    add clickable img selection in product page

--- src/pages/[productId]/index.tsx
+++ src/pages/[productId]/index.tsx
@@ -1,44 +1,63 @@
-
 import { api } from "~/utils/api";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import { useState } from "react";
+// import { QueryObserverSuccessResult } from "react-query"; // Make sure to import the correct type
 
 export default function ProductPage() {
-    
-    // const params = usePathname();
-    // const productQuery = api.shopRouter.getProduct.useQuery({ id: params?.slice(1) });
+  const [showImage, setShowImage] = useState<number>(0);
 
-    const router = useRouter();
-    const productId = typeof router.query.productId === 'string' ? router.query.productId : undefined;
+  const router = useRouter();
+  const productId = typeof router.query.productId === 'string' ? router.query.productId : undefined;
 
-    const productQuery = api.shopRouter.getProduct.useQuery({ id: productId });
-    // console.log(productQuery.data?.title, "this is the product query")
+  const productQuery = api.shopRouter.getProduct.useQuery({ id: productId });
 
+  const currentImg = productQuery.data?.images[showImage] as { src:string, position:string };
+  
+//   console.log(currentImg.src)
+
+    const handleShowImage = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+        const clickedImg = e.target as HTMLImageElement;
+        const index = clickedImg.dataset.index;
+        console.log(index)
+
+        if (index) {
+            setShowImage(parseInt(index));
+        }
+
+    }
+
+  if ('data' in productQuery && productQuery.data) {
     return (
-        <>
-            {productQuery.data && <div className="flex flex-row">
-                
-                {productQuery.data?.images[0] && 
-                <div className="flex flex-row">
-                    <div>
-                        {productQuery.data?.images.map((image, index) => {
-                            return (
-                                <Image src={image.src} width={50} height={50} alt="product image" key={index} />
-                            )
-                        })}
-                    </div>
-                    <Image className="h-64 w-64" src={productQuery.data?.images[0].src} width={500} height={500} alt="product image" />
-                </div>}
-                <h1 className="text-2xl">{productQuery.data?.title}</h1>
-            </div>}
-            
-            
-            
-            
-            
-            {productQuery.error && <div>
-                <h1>This Product does not exist</h1>
-            </div>}
-        </>
-    )
+      <>
+        <div className="flex flex-row">
+          {productQuery.data.images && productQuery.data.images[showImage]?.src && (
+            <div className="flex flex-row">
+              <div className="overflow-y-scroll h-64 pr-3">
+                {productQuery.data.images.map((image, index) => (
+                  <Image onClick={(e) => handleShowImage (e)} src={image.src} width={50} height={50} alt="product image" key={index} data-index={index} />
+                ))}
+              </div>
+              <Image
+                className="h-64 w-64"
+                src={currentImg.src}
+                width={500}
+                height={500}
+                alt="product image"
+              />
+            </div>
+          )}
+          <h1 className="text-2xl">{productQuery.data.title}</h1>
+        </div>
+        {productQuery.error && (
+          <div>
+            <h1>This Product does not exist</h1>
+          </div>
+        )}
+      </>
+    );
+  } else {
+    // Return null or a loading state if productQuery is not yet ready
+    return null;
+  }
 }
